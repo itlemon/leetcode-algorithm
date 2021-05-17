ca42f0b72fa16003caf0c189363ce71606686eaf
itlemon <lemon_jiang@aliyun.com>
2021-05-17 23:43:47 +0800

    mod: No.15 三数之和

--- src/main/java/cn/itlemon/leetcode/array/No15ThreeSum.java
+++ src/main/java/cn/itlemon/leetcode/array/No15ThreeSum.java
@@ -44,12 +44,41 @@ public class No15ThreeSum {
     }
 
     /**
+     * 排序+双指针解法：O(n^2)
      *
-     * @param nums
-     * @return
+     * @param nums 数组
+     * @return 数组列表
      */
     public List<List<Integer>> threeSum2(int[] nums) {
-        return null;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int k = 0; k < nums.length - 2; k++) {
+            if (nums[k] > 0) {
+                break;
+            }
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            int i = k + 1;
+            int j = nums.length - 1;
+            while (i < j) {
+                int sum = nums[k] + nums[i] + nums[j];
+                if (sum > 0) {
+                    while (i < j && nums[j] == nums[--j]) {
+                    }
+                } else if (sum < 0) {
+                    while (i < j && nums[i] == nums[++i]) {
+                    }
+                } else {
+                    result.add(new ArrayList<>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    while (i < j && nums[j] == nums[--j]) {
+                    }
+                    while (i < j && nums[i] == nums[++i]) {
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 }
