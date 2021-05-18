9faf58566a6787be936a127d76244a617915d5b6
itlemon <lemon_jiang@aliyun.com>
2021-05-18 23:53:03 +0800

    mod: Offer.57 两数之和

---
+++ src/main/java/cn/itlemon/leetcode/array/Offer57TwoSum.java
@@ -0,0 +1,34 @@
+package cn.itlemon.leetcode.array;
+
+/**
+ * 剑指 Offer 57. 和为s的两个数字 https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+ *
+ * @author jiangpingping <jiangpingping@kuaishou.com>
+ * Created on 2021-05-18
+ */
+public class Offer57TwoSum {
+
+    /**
+     * 双指针法：O(n)
+     *
+     * @param nums 数组
+     * @param target 目标值
+     * @return 两个数字组成的数组
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum < target) {
+                start++;
+            } else if (sum > target) {
+                end--;
+            } else {
+                return new int[] {nums[start], nums[end]};
+            }
+        }
+        return new int[2];
+    }
+
+}
