23d0287b78b2b88b729dfc5639b2a4d6f15ab2cb
itlemon <lemon_jiang@aliyun.com>
2021-05-20 00:22:52 +0800

    add: No.206 反转链表

---
+++ src/main/java/cn/itlemon/leetcode/list/No206ReverseLinkedList.java
@@ -0,0 +1,41 @@
+package cn.itlemon.leetcode.list;
+
+import cn.itlemon.leetcode.model.ListNode;
+
+/**
+ * No.206 反转链表 https://leetcode-cn.com/problems/reverse-linked-list/
+ *
+ * @author jiangpingping <jiangpingping@kuaishou.com>
+ * Created on 2021-05-20
+ */
+public class No206ReverseLinkedList {
+
+    /**
+     * 迭代法：O(n)
+     *
+     * @param head 头节点
+     * @return 反转后的链表
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 栈：
+     *
+     * @param head 头节点
+     * @return 反转后的链表
+     */
+    public ListNode reverseList2(ListNode head) {
+        return null;
+    }
+
+}
