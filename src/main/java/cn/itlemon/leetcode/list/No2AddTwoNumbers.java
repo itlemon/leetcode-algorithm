d8f6ab069c55126d77f246f9a3a57ca0a42b7b03
itlemon <lemon_jiang@aliyun.com>
2021-05-23 18:48:04 +0800

    add: No.2 两数相加

---
+++ src/main/java/cn/itlemon/leetcode/list/No2AddTwoNumbers.java
@@ -0,0 +1,47 @@
+package cn.itlemon.leetcode.list;
+
+import cn.itlemon.leetcode.model.ListNode;
+
+/**
+ * No.2 两数相加 https://leetcode-cn.com/problems/add-two-numbers/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-05-23
+ */
+public class No2AddTwoNumbers {
+
+    /**
+     * 两个链表数值想加
+     *
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 链表head
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode head = null, tail = null;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+}
