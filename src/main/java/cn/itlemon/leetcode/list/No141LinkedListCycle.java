3f5d575e344fd040434e45403b4c279e11b9bf11
itlemon <lemon_jiang@aliyun.com>
2021-05-27 00:23:06 +0800

    mod: No.141 环形链表

---
+++ src/main/java/cn/itlemon/leetcode/list/No141LinkedListCycle.java
@@ -0,0 +1,57 @@
+package cn.itlemon.leetcode.list;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import cn.itlemon.leetcode.model.ListNode;
+
+/**
+ * No.141 环形链表 https://leetcode-cn.com/problems/linked-list-cycle/
+ *
+ * @author jiangpingping <jiangpingping@kuaishou.com>
+ * Created on 2021-05-27
+ */
+public class No141LinkedListCycle {
+
+    /**
+     * 龟兔赛跑法（快慢指针法）
+     *
+     * @param head 头节点
+     * @return 是否有环
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * HashSet法
+     *
+     * @param head 头节点
+     * @return 是否有环
+     */
+    public boolean hasCycle2(ListNode head) {
+        Set<ListNode> container = new HashSet<>();
+        while (head != null) {
+            boolean addResult = container.add(head);
+            if (!addResult) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+}
