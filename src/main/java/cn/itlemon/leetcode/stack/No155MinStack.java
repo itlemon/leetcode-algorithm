8423e995e160e47a2d5b2dfd85dd0fc32ff4cc8c
itlmeon <lemon_jiang@aliyun.com>
2021-06-06 22:09:01 +0800

    add: No.155 最小栈

---
+++ src/main/java/cn/itlemon/leetcode/stack/No155MinStack.java
@@ -0,0 +1,34 @@
+package cn.itlemon.leetcode.stack;
+
+/**
+ * No.155 最小栈 https://leetcode-cn.com/problems/min-stack/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-06-06
+ */
+public class No155MinStack {
+
+    /**
+     * initialize your data structure here.
+     */
+    public No155MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+
+}
