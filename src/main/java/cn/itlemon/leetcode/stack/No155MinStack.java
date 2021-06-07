f39284f1a04ea4758f42774dd74e3829351e1f71
itlemon <lemon_jiang@aliyun.com>
2021-06-07 23:39:23 +0800

    mod: No.155 最小栈

--- src/main/java/cn/itlemon/leetcode/stack/No155MinStack.java
+++ src/main/java/cn/itlemon/leetcode/stack/No155MinStack.java
@@ -1,5 +1,7 @@
 package cn.itlemon.leetcode.stack;
 
+import java.util.Stack;
+
 /**
  * No.155 最小栈 https://leetcode-cn.com/problems/min-stack/
  *
@@ -8,27 +10,38 @@ package cn.itlemon.leetcode.stack;
  */
 public class No155MinStack {
 
+    Stack<Integer> dataStack;
+
+    /**
+     * 最小值辅助栈
+     */
+    Stack<Integer> minDataStack;
+
     /**
      * initialize your data structure here.
      */
     public No155MinStack() {
-
+        dataStack = new Stack<>();
+        minDataStack = new Stack<>();
+        minDataStack.push(Integer.MAX_VALUE);
     }
 
     public void push(int val) {
-
+        dataStack.push(val);
+        minDataStack.push(Math.min(minDataStack.peek(), val));
     }
 
     public void pop() {
-
+        dataStack.pop();
+        minDataStack.pop();
     }
 
     public int top() {
-
+        return dataStack.peek();
     }
 
     public int getMin() {
-
+        return minDataStack.peek();
     }
 
 }
