e8c5cd67f228d294f9a0cd4832b31ad030be548e
itlemon <lemon_jiang@aliyun.com>
2021-06-09 00:06:30 +0800

    mod: No.84 柱状图中最大的矩形

--- src/main/java/cn/itlemon/leetcode/stack/No84LargestRectangleInHistogram.java
+++ src/main/java/cn/itlemon/leetcode/stack/No84LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package cn.itlemon.leetcode.stack;
 
+import java.util.Stack;
+
 /**
  * No.84 柱状图中最大的矩形 https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
  *
@@ -20,13 +22,50 @@ public class No84LargestRectangleInHistogram {
             int left = i;
             int right = i;
             int height = heights[i];
-            while (left >= 0 && heights[left] >= height) {
+            while (left > 0 && heights[left - 1] >= height) {
                 left--;
             }
-            while (right <= heights.length - 1 && heights[right] >= height) {
+            while (right < heights.length - 1 && heights[right + 1] >= height) {
                 right++;
             }
-            area = Math.max(height * (right - left - 1), area);
+            area = Math.max(height * (right - left + 1), area);
+        }
+        return area;
+    }
+
+    /**
+     * 单调栈+哨兵解法：O(n)
+     *
+     * @param heights 高度数组
+     * @return 最大面积
+     */
+    public int largestRectangleArea2(int[] heights) {
+        if (heights.length == 0) {
+            return 0;
+        }
+        if (heights.length == 1) {
+            return heights[0];
+        }
+        // 面积
+        int area = 0;
+        // 添加哨兵:数组两端各加上一个为0的元素
+        int[] newHeights = new int[heights.length + 2];
+        System.arraycopy(heights, 0, newHeights, 1, heights.length);
+        newHeights[0] = 0;
+        newHeights[heights.length + 1] = 0;
+        heights = newHeights;
+
+        // 单调栈
+        Stack<Integer> stack = new Stack<>();
+        // 加入哨兵，stack中就无需做非空判断，因为0索引对应的高度为0，肯定是数组中最小的
+        stack.push(0);
+        for (int i = 1; i < heights.length; i++) {
+            while (heights[i] < heights[stack.peek()]) {
+                int currentHeight = heights[stack.pop()];
+                int currentWight = i - stack.peek() - 1;
+                area = Math.max(area, currentHeight * currentWight);
+            }
+            stack.push(i);
         }
         return area;
     }
