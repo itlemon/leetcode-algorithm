1723da3596a53de959c0fb61de2cf7299c95b214
itlmeon <lemon_jiang@aliyun.com>
2021-06-12 18:04:44 +0800

    mod: No.496 下一个更大元素

--- src/main/java/cn/itlemon/leetcode/stack/No496NextGreaterElement1.java
+++ src/main/java/cn/itlemon/leetcode/stack/No496NextGreaterElement1.java
@@ -25,7 +25,7 @@ public class No496NextGreaterElement1 {
         Stack<Integer> stack = new Stack<>();
         int[] temp = new int[nums2.length];
         for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums2[i] >= stack.peek()) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
                 stack.pop();
             }
             temp[i] = stack.isEmpty() ? -1 : stack.peek();
@@ -36,5 +36,31 @@ public class No496NextGreaterElement1 {
             index[i] = temp[index[i]];
         }
         return index;
+    }
+
+    /**
+     * 使用map记录nums2中每个元素和下一个更大元素的关系：O(n)
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 数组
+     */
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        // 设置一个Map来存储nums2中每个元素和它下一个更大元素的关系
+        Map<Integer, Integer> map = new HashMap<>((int) (nums2.length / 0.75) + 1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                stack.pop();
+            }
+            // 使用map将nums2中的每个元素与其下一个更大元素关联起来
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[i]);
+        }
+        // 遍历nums1，下一个更大元素
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.get(nums1[i]);
+        }
+        return nums1;
     }
 }
