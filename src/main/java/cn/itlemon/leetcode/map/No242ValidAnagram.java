0d766bb25aa1a08530f933069ce277f3aaa6fafb
itlemon <lemon_jiang@aliyun.com>
2021-06-23 23:58:12 +0800

    add: No.242 有效的字母异位词

---
+++ src/main/java/cn/itlemon/leetcode/map/No242ValidAnagram.java
@@ -0,0 +1,22 @@
+package cn.itlemon.leetcode.map;
+
+/**
+ * No.242 有效的字母异位词 https://leetcode-cn.com/problems/valid-anagram/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-06-23
+ */
+public class No242ValidAnagram {
+
+    /**
+     * 排序
+     *
+     * @param s 字符串1
+     * @param t 字符串2
+     * @return 是否是有效的字母异位词
+     */
+    public boolean isAnagram(String s, String t) {
+
+    }
+
+}
