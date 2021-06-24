5da256560398cd24683d09bc98b173bd44943ef5
itlemon <lemon_jiang@aliyun.com>
2021-06-24 22:59:10 +0800

    add: No.49 字母异位词分组

---
+++ src/main/java/cn/itlemon/leetcode/map/No49GroupAnagrams.java
@@ -0,0 +1,28 @@
+package cn.itlemon.leetcode.map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * No.49 字母异位词分组 https://leetcode-cn.com/problems/group-anagrams/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-06-24
+ */
+public class No49GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> container = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            List<String> list = container.computeIfAbsent(new String(chars), k -> new ArrayList<>());
+            list.add(str);
+        }
+        return new ArrayList<>(container.values());
+    }
+
+}
