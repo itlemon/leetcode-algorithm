89dcbfa316a19caefb80ca3acf9132f26f4fa222
itlemon <lemon_jiang@aliyun.com>
2021-06-25 00:31:04 +0800

    mod: No.94 二叉树的中序遍历

--- src/main/java/cn/itlemon/leetcode/tree/No94BinaryTreeInorderTraversal.java
+++ src/main/java/cn/itlemon/leetcode/tree/No94BinaryTreeInorderTraversal.java
@@ -2,6 +2,7 @@ package cn.itlemon.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import cn.itlemon.leetcode.model.TreeNode;
 
@@ -32,6 +33,27 @@ public class No94BinaryTreeInorderTraversal {
         helper(root.left, result);
         result.add(root.val);
         helper(root.right, result);
+    }
+
+    /**
+     * 迭代法：O(n)
+     *
+     * @param root 根节点
+     * @return 遍历后的列表
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
     }
 
 }
