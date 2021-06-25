8e8eb0fc0f104343298b4a744b3495bfa9832c04
itlmeon <lemon_jiang@aliyun.com>
2021-06-25 23:32:10 +0800

    mod: No.144 二叉树的前序遍历

--- src/main/java/cn/itlemon/leetcode/tree/No144BinaryTreePreorderTraversal.java
+++ src/main/java/cn/itlemon/leetcode/tree/No144BinaryTreePreorderTraversal.java
@@ -2,6 +2,7 @@ package cn.itlemon.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import cn.itlemon.leetcode.model.TreeNode;
 
@@ -34,6 +35,26 @@ public class No144BinaryTreePreorderTraversal {
         helper(root.right, result);
     }
 
+    /**
+     * 迭代法：O(n)
+     *
+     * @param root 根节点
+     * @return 遍历后的列表
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                result.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        return result;
+    }
 
 
 }
