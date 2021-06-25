088de659359621a6486b376349c7c48182f1dab3
itlmeon <lemon_jiang@aliyun.com>
2021-06-26 00:21:03 +0800

    mod: No.145 二叉树的后序遍历

--- src/main/java/cn/itlemon/leetcode/tree/No145BinaryTreePostorderTraversal.java
+++ src/main/java/cn/itlemon/leetcode/tree/No145BinaryTreePostorderTraversal.java
@@ -2,6 +2,7 @@ package cn.itlemon.leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import cn.itlemon.leetcode.model.TreeNode;
 
@@ -32,6 +33,34 @@ public class No145BinaryTreePostorderTraversal {
         helper(root.left, result);
         helper(root.right, result);
         result.add(root.val);
+    }
+
+    /**
+     * 迭代法：O(n)
+     *
+     * @param root 根节点
+     * @return 遍历后的列表
+     */
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                result.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return result;
     }
 
 }
