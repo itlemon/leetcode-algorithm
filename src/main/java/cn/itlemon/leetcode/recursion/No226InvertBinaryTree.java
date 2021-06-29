230064efc65aa7892161a26b991d27517779f4df
itlemon <lemon_jiang@aliyun.com>
2021-06-30 00:14:00 +0800

    mod: No.226 翻转二叉树

--- src/main/java/cn/itlemon/leetcode/recursion/No226InvertBinaryTree.java
+++ src/main/java/cn/itlemon/leetcode/recursion/No226InvertBinaryTree.java
@@ -1,5 +1,7 @@
 package cn.itlemon.leetcode.recursion;
 
+import java.util.LinkedList;
+
 import cn.itlemon.leetcode.model.TreeNode;
 
 /**
@@ -27,6 +29,37 @@ public class No226InvertBinaryTree {
         // 第三步：做好本层的事情
         root.left = right;
         root.right = left;
+        return root;
+    }
+
+    /**
+     * 迭代法，广度优先遍历：O(n)
+     *
+     * @param root 根节点
+     * @return 翻转后的二叉树
+     */
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // 创建一个队列
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // 处理队列中每一个元素的左右子节点
+            TreeNode temp = queue.pollFirst();
+            TreeNode left = temp.left;
+            TreeNode right = temp.right;
+            temp.left = right;
+            temp.right = left;
+            // 分别处理左右子节点
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+        }
         return root;
     }
 }
