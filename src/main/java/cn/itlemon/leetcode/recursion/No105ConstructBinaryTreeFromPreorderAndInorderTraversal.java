9eedd8b433feed91be27153332ba90e26fa362c2
itlemon <lemon_jiang@aliyun.com>
2021-07-05 23:53:45 +0800

    add: No.889 根据前序和后序遍历构造二叉树

--- src/main/java/cn/itlemon/leetcode/recursion/No105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/cn/itlemon/leetcode/recursion/No105ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -21,34 +21,7 @@ public class No105ConstructBinaryTreeFromPreorderAndInorderTraversal {
      * @return 二叉树
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> indexContainer = new HashMap<>((int) (inorder.length / 0.75) + 1);
-        for (int i = 0; i < inorder.length; i++) {
-            indexContainer.put(inorder[i], i);
-        }
-        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length, indexContainer);
-    }
 
-    private TreeNode buildTreeHelper(int[] preorder, int preorderStart, int preorderEnd, int[] inorder,
-            int inorderStart, int inorderEnd, Map<Integer, Integer> map) {
-        // 如果preorder为空，直接返回null
-        if (preorderStart == preorderEnd) {
-            return null;
-        }
-        // 获取根节点
-        int rootVal = preorder[preorderStart];
-        int rootIndex = map.get(rootVal);
-        TreeNode root = new TreeNode(rootVal);
-        // 确定左子树数量，从而可以从前序遍历的列表中找到所有的左子树
-        int leftTreeNodeNum = rootIndex - inorderStart;
-        // 确定左右节点
-        root.left =
-                buildTreeHelper(preorder, preorderStart + 1, preorderStart + leftTreeNodeNum + 1, inorder, inorderStart,
-                        rootIndex, map);
-        root.right =
-                buildTreeHelper(preorder, preorderStart + leftTreeNodeNum + 1, preorderEnd, inorder, rootIndex + 1,
-                        inorderEnd, map);
-        return root;
     }
-
 
 }
---
+++ src/main/java/cn/itlemon/leetcode/recursion/No889ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -0,0 +1,12 @@
+package cn.itlemon.leetcode.recursion;
+
+/**
+ * No.889 根据前序和后序遍历构造二叉树 https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-07-05
+ */
+public class No889ConstructBinaryTreeFromPreorderAndPostorderTraversal {
+
+
+}
