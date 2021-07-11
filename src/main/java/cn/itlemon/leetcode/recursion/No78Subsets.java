b2d76d712cc430a63d1af419a7ce57d1bd254686
itlemon <lemon_jiang@aliyun.com>
2021-07-11 12:06:23 +0800

    add: No.78 子集

---
+++ src/main/java/cn/itlemon/leetcode/recursion/No78Subsets.java
@@ -0,0 +1,26 @@
+package cn.itlemon.leetcode.recursion;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * No.78 子集 https://leetcode-cn.com/problems/subsets/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-07-11
+ */
+public class No78Subsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        Deque<Integer> stack = new LinkedList<>();
+        return backTrack(nums);
+    }
+
+}
