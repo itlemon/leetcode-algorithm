faa4068434c9436e41c6e51814c0ddbce4bfff8e
itlmeon <lemon_jiang@aliyun.com>
2021-07-24 23:30:03 +0800

    mod: No.77 组合

--- src/main/java/cn/itlemon/leetcode/recursion/No77Combinations.java
+++ src/main/java/cn/itlemon/leetcode/recursion/No77Combinations.java
@@ -10,6 +10,13 @@ import java.util.List;
  */
 public class No77Combinations {
 
+    /**
+     * 回溯算法
+     *
+     * @param n 数组[1, n]
+     * @param k k个数
+     * @return 组合
+     */
     public List<List<Integer>> combine(int n, int k) {
         return null;
     }
