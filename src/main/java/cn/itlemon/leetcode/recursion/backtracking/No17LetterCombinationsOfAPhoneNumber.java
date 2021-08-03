305600c8274e15c0fb49e39b690ec16553669db5
itlemon <lemon_jiang@aliyun.com>
2021-08-03 23:41:56 +0800

    mod: No.17 电话号码的字母组合

--- src/main/java/cn/itlemon/leetcode/recursion/backtracking/No17LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/cn/itlemon/leetcode/recursion/backtracking/No17LetterCombinationsOfAPhoneNumber.java
@@ -1,5 +1,6 @@
 package cn.itlemon.leetcode.recursion.backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +11,46 @@ import java.util.List;
  */
 public class No17LetterCombinationsOfAPhoneNumber {
 
-    public List<String> letterCombinations(String digits) {
+    /**
+     * 定义2 ~ 9对应的字母字典
+     */
+    private final String[] letterMap = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
+    /**
+     * 回溯算法
+     *
+     * @param digits 数字
+     * @return 组合
+     */
+    public List<String> letterCombinations(String digits) {
+        // 定义结果集
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0) {
+            return result;
+        }
+        // 定义路径
+        StringBuilder track = new StringBuilder();
+        // 回溯
+        backtracking(digits, 0, track, result);
+        return result;
+    }
+
+    private void backtracking(String digits, int index, StringBuilder track, List<String> result) {
+        // 触发终止条件
+        if (digits.length() == track.length()) {
+            result.add(track.toString());
+            return;
+        }
+
+        String letter = letterMap[digits.charAt(index) - '2'];
+        // 回溯的核心部分
+        for (int i = 0; i < letter.length(); i++) {
+            // 做选择
+            track.append(letter.charAt(i));
+            // 递归
+            backtracking(digits, index + 1, track, result);
+            // 撤销选择，撤销路径中最后一个字母
+            track.deleteCharAt(track.length() - 1);
+        }
     }
 }
