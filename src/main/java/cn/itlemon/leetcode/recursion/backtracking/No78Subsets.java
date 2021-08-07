de0959178c88646d4464aaa5464b0cc97e91b301
itlemon <lemon_jiang@aliyun.com>
2021-08-07 17:21:40 +0800

    mod: No.78 子集

--- src/main/java/cn/itlemon/leetcode/recursion/backtracking/No78Subsets.java
+++ src/main/java/cn/itlemon/leetcode/recursion/backtracking/No78Subsets.java
@@ -1,6 +1,5 @@
 package cn.itlemon.leetcode.recursion.backtracking;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -14,17 +13,42 @@ import java.util.List;
  */
 public class No78Subsets {
 
+    /**
+     * 回溯
+     *
+     * @param nums 数组
+     * @return 子集列表
+     */
     public List<List<Integer>> subsets(int[] nums) {
+        // 定义结果集
         List<List<Integer>> result = new ArrayList<>();
         if (nums.length == 0) {
             return result;
         }
-        Deque<Integer> stack = new LinkedList<>();
-        return backTrack(nums);
+        // 定义路径
+        Deque<Integer> track = new LinkedList<>();
+        // 通过for循环确定每次组合的个数
+        for (int i = 0; i <= nums.length; i++) {
+            backtracking(nums, i, 0, track, result);
+        }
+        return result;
     }
 
-    private List<List<Integer>> backTrack(int[] nums) {
-        return null;
+    private void backtracking(int[] nums, int count, int start, Deque<Integer> track, List<List<Integer>> result) {
+        // 触发终止条件
+        if (count == track.size()) {
+            result.add(new ArrayList<>(track));
+        }
+
+        // 回溯的核心部分
+        for (int i = start; i < nums.length; i++) {
+            // 做选择
+            track.addLast(nums[i]);
+            // 递归
+            backtracking(nums, count, i + 1, track, result);
+            // 做选择
+            track.removeLast();
+        }
     }
 
 }
