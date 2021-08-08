b130cb4dd6a9236ee268a078ed893aad06b2b512
itlmeon <lemon_jiang@aliyun.com>
2021-08-08 22:26:56 +0800

    add: No.51 N皇后

---
+++ src/main/java/cn/itlemon/leetcode/recursion/backtracking/No51NQueens.java
@@ -0,0 +1,17 @@
+package cn.itlemon.leetcode.recursion.backtracking;
+
+import java.util.List;
+
+/**
+ * No.51 N皇后 https://leetcode-cn.com/problems/n-queens/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-08-08
+ */
+public class No51NQueens {
+
+    public List<List<String>> solveNQueens(int n) {
+        return null;
+    }
+
+}
