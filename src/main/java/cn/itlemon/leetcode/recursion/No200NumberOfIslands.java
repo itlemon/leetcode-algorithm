77a522e9a2cce4eaa0645ec2d5c26a49116d53a0
itlmeon <lemon_jiang@aliyun.com>
2021-08-22 22:49:37 +0800

    mod: No.200 岛屿数量

--- src/main/java/cn/itlemon/leetcode/recursion/No200NumberOfIslands.java
+++ src/main/java/cn/itlemon/leetcode/recursion/No200NumberOfIslands.java
@@ -8,8 +8,67 @@ package cn.itlemon.leetcode.recursion;
  */
 public class No200NumberOfIslands {
 
+    /**
+     * DFS解决岛屿数量问题
+     *
+     * @param grid 二维数组
+     * @return 岛屿数量
+     */
     public int numIslands(char[][] grid) {
+        // 定义结果
+        int result = 0;
+        // 循环遍历
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    traverse(grid, i, j);
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
 
+    /**
+     * DFS
+     *
+     * @param grid 网格
+     * @param r 行坐标
+     * @param c 列坐标
+     */
+    private void traverse(char[][] grid, int r, int c) {
+        // 触发终止条件：判断当前遍历的格子是否在网格内
+        if (!isInGrid(grid, r, c)) {
+            return;
+        }
+
+        // 本层要做的事情
+        // 如果格子不是岛屿，那么直接返回，相当于剪枝，防止重复操作
+        if (grid[r][c] != '1') {
+            return;
+        }
+
+        // 做标记，将本层遍历后的格子标记为-1
+        grid[r][c] = '2';
+
+        // 递归访问上、下、左、右四个相邻格子
+        traverse(grid, r - 1, c);
+        traverse(grid, r + 1, c);
+        traverse(grid, r, c - 1);
+        traverse(grid, r, c + 1);
+    }
+
+    /**
+     * 判断指定坐标是否在网格内
+     *
+     * @param grid 表示网格的二维数组
+     * @param r 行坐标
+     * @param c 列坐标
+     * @return 是否在网格内
+     */
+    private boolean isInGrid(char[][] grid, int r, int c) {
+        return r >= 0 && r < grid.length
+                && c >= 0 && c < grid[0].length;
     }
 
 }
