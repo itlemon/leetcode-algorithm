dc96b469439ee354a524b4d4f5b94298a050ebce
itlemon <lemon_jiang@aliyun.com>
2021-09-01 00:04:35 +0800

    add: No.122 买卖股票的最佳时机

---
+++ src/main/java/cn/itlemon/leetcode/greedy/No122BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,15 @@
+package cn.itlemon.leetcode.greedy;
+
+/**
+ * No.122 买卖股票的最佳时机 II https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-09-01
+ */
+public class No122BestTimeToBuyAndSellStock2 {
+
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+}
