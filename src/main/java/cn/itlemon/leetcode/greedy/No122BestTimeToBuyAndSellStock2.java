5bc35eeead1075a2e8187a19868ee91270ab7e2f
itlemon <lemon_jiang@aliyun.com>
2021-09-02 22:44:20 +0800

    mod: No.122 买卖股票的最佳时机

--- src/main/java/cn/itlemon/leetcode/greedy/No122BestTimeToBuyAndSellStock2.java
+++ src/main/java/cn/itlemon/leetcode/greedy/No122BestTimeToBuyAndSellStock2.java
@@ -8,8 +8,35 @@ package cn.itlemon.leetcode.greedy;
  */
 public class No122BestTimeToBuyAndSellStock2 {
 
+    /**
+     * 贪心算法
+     *
+     * @param prices 股票价格
+     * @return 最大利润
+     */
     public int maxProfit(int[] prices) {
-        return 0;
+        int length = prices.length;
+        if (length <= 1) {
+            return 0;
+        }
+
+        // 定义结果
+        int result = 0;
+        int sum = 0;
+        for (int i = 0; i < length - 1; i++) {
+            if (prices[i] <= prices[i + 1]) {
+                sum += (prices[i + 1] - prices[i]);
+                if (i == length - 2) {
+                    // 说明遍历到了最后
+                    result += sum;
+                }
+            } else {
+                // 出现价格下降，这里就卖出，并设置sum为0，重新开始计算利润
+                result += sum;
+                sum = 0;
+            }
+        }
+        return result;
     }
 
 }
