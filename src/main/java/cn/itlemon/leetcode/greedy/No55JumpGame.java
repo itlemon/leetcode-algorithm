70327c71d9bd28571de99810fe84328ea2689d8b
itlemon <lemon_jiang@aliyun.com>
2021-09-02 23:47:09 +0800

    add: No.55 跳跃游戏

---
+++ src/main/java/cn/itlemon/leetcode/greedy/No55JumpGame.java
@@ -0,0 +1,15 @@
+package cn.itlemon.leetcode.greedy;
+
+/**
+ * No.55 跳跃游戏 https://leetcode-cn.com/problems/jump-game/
+ *
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2021-09-02
+ */
+public class No55JumpGame {
+
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+
+}
